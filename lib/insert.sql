299f83c5dcd9977ff0c2c9c89201f35624b4bfdb
George <gnm7208@gmail.com>
2025-11-27 12:41:26 +0300

    `Added 8 bear records to the bears table in the database.`

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'brown', 'calm', 1),
+  ('Rowdy', 8, 'M', 'black', 'aggressive', 1),
+  ('Tabitha', 3, 'F', 'white', 'gentle', 1),
+  ('Sergeant Brown', 12, 'M', 'brown', 'strict', 1),
+  ('Melissa', 6, 'F', 'golden', 'friendly', 1),
+  ('Grinch', 10, 'M', 'green', 'grumpy', 1),
+  ('Wendy', 4, 'F', 'tan', 'playful', 1),
+  (NULL, 2, 'M', 'gray', 'shy', 1);
